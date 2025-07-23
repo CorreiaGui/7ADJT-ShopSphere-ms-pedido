ab3bdc3570dfbdda33ec2e4bfe312ab66722b30c
~ Gui Correia <103298767+CorreiaGui@users.noreply.github.com>
2025-07-23 19:09:26 -0300

    fix: apagando dados controller

--- src/main/java/br/com/fiap/ms/pedidoservice/controller/PedidoController.java
+++ src/main/java/br/com/fiap/ms/pedidoservice/controller/PedidoController.java
@@ -1,43 +1,12 @@
 package br.com.fiap.ms.pedidoservice.controller;
 
-import br.com.fiap.ms.pedidoservice.gateway.database.jpa.entity.PedidoEntity;
-import br.com.fiap.ms.pedidoservice.gateway.external.cliente.ClienteFeignClient;
-import br.com.fiap.ms.pedidoservice.gateway.external.cliente.response.ClienteJsonResponse;
-import br.com.fiap.ms.pedidoservice.gateway.external.produto.entity.ProdutoResponse;
-import br.com.fiap.ms.pedidoservice.gateway.external.produto.service.ProdutoService;
-import br.com.fiap.ms.pedidoservice.usecase.PedidoPersistenceService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-/**
- * Controlador REST para gerenciar pedidos.
- * Permite buscar pedidos por ID e testar a conectividade com o serviço.
- */
 @RestController
 @RequestMapping("/pedidos")
 @RequiredArgsConstructor
 public class PedidoController {
 
-    private final PedidoPersistenceService service;
-    private final ProdutoService produtoService;
-    private final ClienteFeignClient clienteFeignClient;
 
-    @GetMapping("/produto/{sku}")
-    public ResponseEntity<ProdutoResponse> buscarProduto(@PathVariable String sku) {
-        ProdutoResponse produto = produtoService.buscarPorSku(sku);
-        return ResponseEntity.ok(produto);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<PedidoEntity> buscarPorId(@PathVariable Long id) {
-        PedidoEntity pedido = service.buscarPorId(id);
-        return pedido != null ? ResponseEntity.ok(pedido) : ResponseEntity.notFound().build();
-    }
-
-    // Teste
-    @GetMapping("/ping")
-    public ResponseEntity<String> ping() {
-        return ResponseEntity.ok("pong");
-    }
 }
