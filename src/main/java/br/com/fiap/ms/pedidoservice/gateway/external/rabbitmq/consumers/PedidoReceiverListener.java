5fee986096267a3e41a5990c993688063923c03b
~ Gui Correia <103298767+CorreiaGui@users.noreply.github.com>
2025-08-01 10:20:42 -0300

    fix: ajuste no PedidoReceiverListener

--- src/main/java/br/com/fiap/ms/pedidoservice/gateway/external/rabbitmq/consumers/PedidoReceiverListener.java
+++ src/main/java/br/com/fiap/ms/pedidoservice/gateway/external/rabbitmq/consumers/PedidoReceiverListener.java
@@ -2,6 +2,8 @@ package br.com.fiap.ms.pedidoservice.gateway.external.rabbitmq.consumers;
 
 import br.com.fiap.ms.pedidoservice.controller.json.PedidoRequestJson;
 import br.com.fiap.ms.pedidoservice.usecase.CriarPedidoUseCase;
+import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Component;
  * Listener para receber pedidos da fila RabbitMQ.
  * Processa os pedidos recebidos utilizando o caso de uso PedidoReceiverUseCase.
  */
+@Slf4j
 @Component
 public class PedidoReceiverListener {
 
@@ -18,6 +21,14 @@ public class PedidoReceiverListener {
 
     @RabbitListener(queues = "novo-pedido-queue")
     public void receberPedido(PedidoRequestJson json) {
-        usecase.criarPedido(json);
+        try {
+            usecase.criarPedido(json);
+        } catch (FeignException.BadRequest e) {
+            log.warn("Erro de regra de negócio: {}", e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            log.error("Erro inesperado", e);
+            throw e;
+        }
     }
 }
